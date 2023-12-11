c4f91d292ed64adf1b8d518d728c7ed24ca096bd
Gopichand Gujja <gopichand.g0693@gmail.com>
2023-12-11 22:10:19 +0530

    added complete codes for rec fun

--- functional_programming/recfun/src/main/scala/recfun/RecFun.scala
+++ functional_programming/recfun/src/main/scala/recfun/RecFun.scala
@@ -1,11 +1,8 @@
 package recfun
 
-import recfun.RecFun.balance
-
 object RecFun extends RecFunInterface:
 
   def main(args: Array[String]): Unit =
-    println()
     println("Pascal's Triangle")
     for row <- 0 to 10 do
       for col <- 0 to row do
@@ -16,39 +13,27 @@ object RecFun extends RecFunInterface:
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int =
-    if c==0 || c==r then 1
-    else pascal(c-1,r-1)+pascal(c,r-1)
+    if c == 0 || c == r then 1 else pascal(c,r-1) + pascal(c-1,r-1)
 
   /**
-   * Exercise 2
+   * Exercx`ise 2
    */
   def balance(chars: List[Char]): Boolean =
+    def innerFunc(counter: Int, chrs : List[Char]): Boolean =
+      if chrs.isEmpty then counter == 0
+      else if counter <0 then false
+      else if chrs.head == '(' then innerFunc(counter+1, chrs.tail)
+      else if chrs.head == ')' then innerFunc(counter-1, chrs.tail)
+      else innerFunc(counter, chrs.tail)
 
-    def innerFuntion(chars: List[Char], openings:Int): Boolean =
-      if (chars.isEmpty) {
-        openings == 0
-      } else {
-        val h = chars.head
-        val number =
-          if h == '(' then openings+1
-          else if h == ')' then openings-1
-          else openings
-        if number >= 0  then innerFuntion(chars.tail,number)
-        else false
-      }
-    innerFuntion(chars,0)
+    innerFunc(0,chars)
 
   println(balance("())(".toList))
-
   /**
    * Exercise 3
    */
   def countChange(money: Int, coins: List[Int]): Int =
-    if money == coins.head  then 1
-    else if money-coins.head < 0 then 0
-    else if coins.tail.isEmpty then countChange(money-coins.head, coins)
-    else { val remaining_money = money-coins.head
-        countChange(remaining_money, coins) + countChange(money, coins.tail) }
-
+    if money == 0 then 1
+    else if (money < 0 || coins.isEmpty) 0
+    else countChange(money-coins.head, coins) + countChange(money, coins.tail)
   println(countChange(7, List(1,2)))
-
